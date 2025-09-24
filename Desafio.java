a5b79293bc6f2477ee583895fc4f5940d83a1189
KalondraAv <avaromavkalondra@gmail.com>
2025-09-24 14:18:22 -0400

    segundo envio
    Aula de POO - Desafio: Personagens

---
+++ Desafio.java
@@ -0,0 +1,9 @@
+package desafioProd;
+
+public class Desafio {
+   public static void main(String[] args) {
+	   double preco, tamanho;
+	   int anoVal;
+	   String material, categoria, funcao, marca, tipo;
+   }
+}
