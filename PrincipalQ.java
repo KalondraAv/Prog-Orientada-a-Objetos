41657f0d40309938e4cd00d592312dd00989fc4a
KalondraAv <avaromavkalondra@gmail.com>
2025-10-16 16:07:01 -0400

    Add files via upload

---
+++ PrincipalQ.java
@@ -0,0 +1,15 @@
+package atv;
+
+public class PrincipalQ {
+
+	public static void main(String[] args) {
+		/** Classe: Qudrado
+	    Atributos: Área, Perímetro
+	    Métodos: Calcular área, Calcular perímetro, Imprimir.
+	**/
+		Quadrado q1 = new Quadrado(lado:4);
+		q1.calcularArea();
+		q1;calcularPerim();
+		q1.imprimir();
+	}
+}
