5d6a5c259dd2a036b5a0ae543c36af747941a3ad
KalondraAv <avaromavkalondra@gmail.com>
2025-11-06 17:16:20 -0400

    Add files via upload

---
+++ PrincipalR.java
@@ -0,0 +1,19 @@
+package formasGeom;
+
+public class PrincipalR {
+
+	public static void main(String[] args) {
+		/** Classe: Qudrado
+	    Atributos: Área, Perímetro
+	    Métodos: Calcular área, Calcular perímetro, Imprimir.
+	**/
+		Retangulo r1 = new Retangulo(comp:4);
+		Retangulo r1 = new Retangulo(larg:7);
+		r1.calcularArea();
+		r1;calcularPerim();
+		r1.imprimir();
+	}
+
+	}
+
+}
